3099be441377dff3eff3bdf13fa3d0cc97ce34e1
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-11 15:40:06 +0100

    Added a search compoent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {" "}
-        HELLO
-        <a href="#" className="btn btn-primary">
-          {" "}
-          Search{" "}
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <Dictionary />
+        <footer className="App-footer">Coded by Anna</footer>
+      </div>
     </div>
   );
 }
