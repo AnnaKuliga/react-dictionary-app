a111c1c93096f0b651a08fae2124aca591ac23e0
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-13 14:19:12 +0100

    added css to the layout

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -16,22 +17,41 @@ export default function Dictionary() {
     //console.log(response.data.meanings[0].definition); to show only the definition of the first meaning
     setResults(response.data);
   }
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     //documentation https://www.shecodes.io/learn/apis/dictionary
     let apiKey = `5fb4oa610201e8b3c770fffbaee96fft`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
-    console.log(apiUrl);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              autoFocus={true}
+              onChange={handleKeywordChange}
+            />
+          </form>
+          <div className="hint">
+            Suggested words: sunset, dry, sophisticated, vicious...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
