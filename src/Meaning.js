6c3da88310c3bb018ef46f6639df934d5a59708a
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-13 14:19:54 +0100

    added sections

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,13 +5,14 @@ export default function Meaning(props) {
   console.log(props);
   return (
     <div className="Meaning">
-      <h4>{props.meaning.partOfSpeech}</h4>
-      <p>
-        {props.meaning.definition} <br />
-        <strong>Example:</strong> <em> {props.meaning.example} </em>
-        <Synonyms synonyms={props.meaning.synonyms} />
-        <hr />
-      </p>
+      <section>
+        <h4>{props.meaning.partOfSpeech}</h4>
+        <p>
+          {props.meaning.definition} <br />
+          <strong>Example:</strong> <em> {props.meaning.example} </em>
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </p>
+      </section>
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Meaning from "./Meaning";
+import "./Results.css";
 
 export default function Results(props) {
   console.log(props.response);
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
-        <h5>{props.results.phonetic}</h5>{" "}
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
+        <section>
+          <h2>{props.results.word}</h2>
+          <h5>{props.results.phonetic}</h5>{" "}
+        </section>
+        <section>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </section>
       </div>
     );
   } else {
